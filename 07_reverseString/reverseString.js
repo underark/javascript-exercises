872b121d846b5e0dc3cc98781d13f6d233a15d8e
alex <peter.alex.morris@gmail.com>
2025-08-17 22:09:59 +0900

    Added solution for 07
    Split string into character array, reversed, and rejoined to reverse a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    return stringArray.reverse().join('');
 };
 
 // Do not edit below this line
