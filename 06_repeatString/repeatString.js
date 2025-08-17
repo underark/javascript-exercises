0a6dc32435168098598e9e8029c329e264685b5b
alex <peter.alex.morris@gmail.com>
2025-08-17 22:02:57 +0900

    Added solution to 06_repeatString
    Used a loop to concatenate a string a given number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
 
+    let repeat = "";
+    for (let i = 0; i < number; i++) {
+        repeat += string;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
