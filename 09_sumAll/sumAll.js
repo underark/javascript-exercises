47ce3b2637cbf9a8bb55dbfa781c990d37505a03
alex <peter.alex.morris@gmail.com>
2025-08-17 22:57:53 +0900

    Added solution for 09
    Used for loop to go through values in a range and add them to total with various error checks

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    let low = Math.min(num1, num2);
+    let high = Math.max(num1, num2);
 
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
+
+    if (low < 0 || high < 0) {
+        return "ERROR";
+    }
+
+    if (low % 1 != 0 || high % 1 != 0) {
+        return "ERROR";
+    }
+
+    for (let i = low; i <= high; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
