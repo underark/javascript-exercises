8cfcd605959368e9b638228b18f5e313d02c4a68
alex <peter.alex.morris@gmail.com>
2025-08-17 23:13:12 +0900

    Added solution for 11
    Used toFixed and parseFloat methods to convert temperatures with 1 decimal place

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  return Number.parseFloat(((degreeF - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  return Number.parseFloat(((degreeC * 9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
