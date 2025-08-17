79a5903c9ab0d4e54b529e887bb872b97c37b750
alex <peter.alex.morris@gmail.com>
2025-08-17 22:33:52 +0900

    Added solution for 08
    Checked whether item is contained in array parameter and push it if not included

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    let newArray = [];
+    array.forEach((member) => {
+        if (!items.includes(member)) {
+            newArray.push(member);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
