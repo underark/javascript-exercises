89160315cf84ce0707cab6cf4b01c80677224327
alex <peter.alex.morris@gmail.com>
2025-08-20 23:07:05 +0900

    Added solution for 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS"
+    }
+    let counter = 2;
+    let current = 1;
+    let previous = 1;
 
+    while (counter < number) {
+        current = current + previous;
+        previous = current - previous;
+        counter++;
+    }
+    return number == 0 ? 0 : current;
 };
 
 // Do not edit below this line
