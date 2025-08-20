a4afe7f8812dcf9d5b2af102cc13a649a6ddf974
alex <peter.alex.morris@gmail.com>
2025-08-21 00:05:07 +0900

    Added comment on 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -5,6 +5,7 @@ const palindromes = function (string) {
     const last = newString.length - 1;
     const mid = Math.floor(newString.length / 2);
 
+    // Could have just used reverse and join to test this, but this is more fun
     for (let i = 0; i < mid; i++) {
         if (newString[i] != newString[last - i]) return false;
     }
