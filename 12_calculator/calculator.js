404b2d82c1cca1a90b27b64c14f5b32a5a16b965
alex <peter.alex.morris@gmail.com>
2025-08-20 22:02:53 +0900

    Added solutions for 11 and 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  let total = n1;
+  for (let i = 0; i < n2 - 1; i++) {
+    total = total * n1;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  for (let i = 1; i < number; i++) {
+    total = total * (i + 1);
+  }
+  return total;
 };
 
 // Do not edit below this line
