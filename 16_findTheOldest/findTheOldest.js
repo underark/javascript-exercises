1361339d87f7d16b8c682c2efa3a1a07415b2d76
alex <peter.alex.morris@gmail.com>
2025-08-21 00:02:56 +0900

    Added solution for 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((previous, current) => {
+        const year = new Date().getFullYear();
+        const previousAge = previous.yearOfDeath != null ? previous.yearOfDeath - previous.yearOfBirth : year - previous.yearOfBirth
+        const currentAge = current.yearOfDeath != null ? current.yearOfDeath - current.yearOfBirth : year - current.yearOfBirth;
+        // console.log(`person is: ${previous.name} age ${previousAge}`);
+        return previousAge > currentAge ? previous : current;
+    });
 };
 
 // Do not edit below this line
