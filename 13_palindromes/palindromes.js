5d2a2e61ce7b2f20e511402d747f24809508cd70
alex <peter.alex.morris@gmail.com>
2025-08-20 22:52:37 +0900

    Added solution for 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.replace(/\W/gi, '');
+    newString = newString.toLowerCase();
 
+    const last = newString.length - 1;
+    const mid = Math.floor(newString.length / 2);
+
+    for (let i = 0; i < mid; i++) {
+        if (newString[i] != newString[last - i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
